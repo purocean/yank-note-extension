696672176074ec93b353c3ed1c851174efdd6f34
purocean <purocean@gmail.com>
2022-05-09 12:38:05 +0800

    chore: fix bump-api-version script

--- scripts/bump-api-version.sh
+++ scripts/bump-api-version.sh
@@ -15,6 +15,6 @@ cd ../../;
 pnpm update -r "$PACKAGE_NAME";
 
 cd packages/create-extension/template-typescript;
-cat package.json | sed -e 's/"$PACKAGE_NAME": ".*"/"$PACKAGE_NAME": "^'$VERSION'"/' > package.json.new \
+cat package.json | sed -e 's/"'${PACKAGE_NAME//\//\\/}'": ".*"/"'${PACKAGE_NAME//\//\\/}'": "^'$VERSION'"/' > package.json.new \
     && rm package.json && mv package.json.new package.json;
 git add -u && git commit -m "chore: bump $PACKAGE_NAME to $VERSION";
