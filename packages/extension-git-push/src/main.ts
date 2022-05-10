4481985294331b7fcda39e2ed7d6ea0677af8d27
purocean <purocean@gmail.com>
2022-05-10 21:31:04 +0800

    chore: change extension name

--- packages/extension-git-push/src/main.ts
+++ packages/extension-git-push/src/main.ts
@@ -1,33 +1,33 @@
 import { registerPlugin } from '@yank-note/runtime-api'
 
-const extensionName = 'yank-note-extension-git-push'
+const extensionId = __EXTENSION_ID__
 
 registerPlugin({
-  name: extensionName,
+  name: extensionId,
   register (ctx) {
     ctx.i18n.mergeLanguage('en', {
-      [extensionName]: {
+      [extensionId]: {
         'git-push': 'Git Push',
         'not-support': 'Yank Note downloaded from the Mac Apple Store does not support this extension.'
       },
     })
 
     ctx.i18n.mergeLanguage('zh-CN', {
-      [extensionName]: {
+      [extensionId]: {
         'git-push': 'Git 推送',
         'not-support': '从 Mac Apple Store 中下载的应用不支持此拓展。'
       },
     })
 
     ctx.statusBar.tapMenus(menus => {
-      menus[extensionName] = {
-        id: extensionName,
+      menus[extensionId] = {
+        id: extensionId,
         position: 'left',
-        title: ctx.i18n.t(`${extensionName}.git-push`),
+        title: ctx.i18n.t(`${extensionId}.git-push`),
         onClick: async () => {
           if (ctx.args.FLAG_MAS) {
             if (await ctx.ui.useModal().confirm({
-              content: ctx.i18n.t(`${extensionName}.not-support`),
+              content: ctx.i18n.t(`${extensionId}.not-support`),
             })) {
               window.open('https://github.com/purocean/yn/issues/65#issuecomment-1065799677')
             }
