d040c7c9e8df20fac273e049c2bdc2e6872591b1
purocean <purocean@gmail.com>
2022-05-12 18:31:58 +0800

    feat: add OpenAI extension

---
+++ packages/extension-openai/README.md
@@ -0,0 +1,13 @@
+# OpenAI
+
+This extension integrates [OpenAI](https://openai.com/) into Yank Note.
+
+## Usage
+
+Select some text and press keyboard shortcut to trigger auto completion:
+- **macOS:** `Cmd` + `Option` + `.`
+- **Windows:** `Ctrl` + `Alt` + `.`
+
+## Screenshots
+
+![image](https://user-images.githubusercontent.com/7115690/168034966-12fd1419-7fac-4dfa-82c4-f9b4ce4f74a8.png)
---
+++ packages/extension-openai/src/main.ts
@@ -0,0 +1,298 @@
+import { Ctx, ctx, registerPlugin } from '@yank-note/runtime-api'
+import Monaco from '@yank-note/runtime-api/types/types/third-party/monaco-editor'
+
+const { getLogger } = ctx.utils
+
+const actionName = 'plugin.editor-openai.trigger'
+const settingKeyToken = 'plugin.editor-openai.api-token'
+const settingKeyEngine = 'plugin.editor-openai.engine-id'
+const settingKeyMode = 'plugin.editor-openai.mode'
+const settingKeyMaxTokens = 'plugin.editor-openai.max-tokens'
+const settingKeyRange = 'plugin.editor-openai.range'
+const settingKeyArgs = 'plugin.editor-openai.args-json'
+const defaultEngine = 'text-davinci-002'
+const extensionId = __EXTENSION_ID__
+
+class CompletionProvider implements Monaco.languages.InlineCompletionsProvider {
+  private readonly monaco: typeof Monaco
+  private readonly ctx: Ctx
+  private logger = getLogger(extensionId)
+
+  constructor (monaco: typeof Monaco, ctx: Ctx) {
+    this.monaco = monaco
+    this.ctx = ctx
+  }
+
+  freeInlineCompletions (): void {
+    this.ctx.ui.useToast().hide()
+  }
+
+  handleItemDidShow (): void {
+    this.ctx.ui.useToast().hide()
+  }
+
+  public async provideInlineCompletions (model: Monaco.editor.IModel, position: Monaco.Position, context: Monaco.languages.InlineCompletionContext): Promise<Monaco.languages.InlineCompletions> {
+    if (context.triggerKind !== this.monaco.languages.InlineCompletionTriggerKind.Explicit) {
+      return { items: [] }
+    }
+
+    this.ctx.ui.useToast().show('info', 'OpenAI: Loading...', 10000)
+
+    const range = this.ctx.setting.getSetting(settingKeyRange, 256)
+
+    let prefix = ''
+    let suffix = ''
+
+    // get selection of editor model
+    const selection = this.ctx.editor.getEditor().getSelection()
+    const selectionText = selection && model.getValueInRange(selection)
+
+    if (selectionText) {
+      prefix = selectionText
+      position = selection!.getEndPosition()
+    } else {
+      const contentPrefix = model.getValueInRange(new this.monaco.Range(
+        1,
+        1,
+        position.lineNumber,
+        position.column,
+      ))
+
+      const maxLine = model.getLineCount()
+      const maxColumn = model.getLineMaxColumn(maxLine)
+
+      const contentSuffix = model.getValueInRange(new this.monaco.Range(
+        position.lineNumber,
+        position.column,
+        maxLine,
+        maxColumn,
+      ))
+
+      prefix = contentPrefix.substring(Math.max(0, contentPrefix.length - range))
+      suffix = contentSuffix.substring(0, range)
+    }
+
+    this.logger.debug('provideInlineCompletions', range, prefix, suffix)
+
+    return {
+      items: await this.provideSuggestions(prefix, suffix, position)
+    }
+  }
+
+  private async provideSuggestions (prompt: string, suffix: string, position: Monaco.Position): Promise<Monaco.languages.InlineCompletion[]> {
+    const range = new this.monaco.Range(
+      position.lineNumber,
+      position.column,
+      position.lineNumber,
+      position.column,
+    )
+
+    const token = this.ctx.setting.getSetting(settingKeyToken, '')
+
+    if (token.length < 40) {
+      this.ctx.ui.useToast().show('warning', 'OpenAI: No API token')
+      return []
+    }
+
+    const headers = {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`
+    }
+
+    let args = {}
+    try {
+      args = JSON.parse(this.ctx.setting.getSetting(settingKeyArgs, '{}'))
+    } catch (e: any) {
+      this.ctx.ui.useToast().show('warning', `OpenAI: Custom Arguments Error "${e.message}"`, 5000)
+      throw e
+    }
+
+    const mode = this.ctx.setting.getSetting(settingKeyMode, 'insert')
+    const maxTokens = this.ctx.setting.getSetting(settingKeyMaxTokens, 256)
+
+    const body = {
+      temperature: 0.3,
+      n: 1,
+      top_p: 1,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+      // stop: '\n',
+      ...args,
+      prompt: prompt,
+      max_tokens: maxTokens,
+      suffix: mode === 'insert' && suffix.trim() ? suffix : undefined
+    }
+
+    const engineId = this.ctx.setting.getSetting('plugin.editor-openai.engine-id', defaultEngine)
+
+    this.logger.debug('provideSuggestions', 'request', engineId, body)
+
+    try {
+      const res = await this.ctx.api.proxyRequest(
+        `https://api.openai.com/v1/engines/${engineId}/completions`,
+        { headers, body: JSON.stringify(body), method: 'post' },
+        true
+      ).then(x => x.json())
+
+      this.logger.debug('provideSuggestions', 'result', res)
+
+      if (!res.choices) {
+        this.ctx.ui.useToast().show('warning', JSON.stringify(res), 5000)
+        return []
+      }
+
+      return res.choices.map((x: any) => ({
+        text: x.text,
+        range,
+      }))
+    } catch (error: any) {
+      this.ctx.ui.useToast().show('warning', error.message || `${error}`, 5000)
+      this.logger.error('provideSuggestions', 'error', error)
+      throw error
+    }
+  }
+}
+
+registerPlugin({
+  name: extensionId,
+  register (ctx) {
+    ctx.editor.whenEditorReady().then(({ monaco }) => {
+      monaco.languages.registerInlineCompletionsProvider(
+        'markdown',
+        new CompletionProvider(monaco, ctx)
+      )
+    })
+
+    ctx.i18n.mergeLanguage('en', {
+      [extensionId]: {
+        'openai-complete': 'OpenAI Complete',
+        'api-token': 'Api Token',
+        'api-token-desc': 'You can get your api token from <a target="_blank" href="http://openai.com">openai.com</a>',
+        'engine-id': 'Engine Id',
+        'engine-id-desc': 'Please refer to <a target="_blank" href="https://beta.openai.com/docs/engines/overview/">Engine Overview</a>',
+        mode: 'Mode',
+        range: 'Characters Range',
+        'range-desc': 'Context characters range',
+        'max-tokens': 'Max Tokens',
+        'args-json': 'Custom Arguments',
+        'args-json-desc': 'Query parameters, JSON string like {"temperature": 0.3}',
+      },
+    })
+
+    ctx.i18n.mergeLanguage('zh-CN', {
+      'openai-complete': 'OpenAI 自动补全',
+      'api-token': 'Api Token',
+      'api-token-desc': '你可以从 <a target="_blank" href="http://openai.com">openai.com</a> 获取',
+      'engine-id': 'Engine Id',
+      'engine-id-desc': '请参考 <a target="_blank" href="https://beta.openai.com/docs/engines/overview/">Engine Overview</a>',
+      mode: '模式',
+      range: '字符范围',
+      'range-desc': '上下文字符范围',
+      'max-tokens': 'Max Tokens',
+      'args-json': '自定义参数',
+      'args-json-desc': '请求参数，JSON 字符串如 {"temperature": 0.3}',
+    })
+
+    ctx.setting.changeSchema((schema) => {
+      schema.groups.push({ label: 'OpenAI' as any, value: 'openai' as any })
+
+      schema.properties[settingKeyMode] = {
+        title: `T_${extensionId}.mode` as any,
+        type: 'string',
+        defaultValue: 'insert',
+        enum: ['insert', 'complete'],
+        group: 'openai' as any,
+        required: true,
+      }
+
+      schema.properties[settingKeyEngine] = {
+        title: `T_${extensionId}.engine-id` as any,
+        description: `T_${extensionId}.engine-id-desc` as any,
+        type: 'string',
+        defaultValue: defaultEngine,
+        group: 'openai' as any,
+        enum: [
+          'text-davinci-002',
+          'text-curie-001',
+          'text-babbage-001',
+          'text-ada-001',
+        ],
+        required: true,
+      }
+
+      schema.properties[settingKeyToken] = {
+        title: `T_${extensionId}.api-token` as any,
+        description: `T_${extensionId}.api-token-desc` as any,
+        type: 'string',
+        defaultValue: '',
+        group: 'openai' as any,
+        options: {
+          inputAttributes: { placeholder: 'sk-' + 'x'.repeat(10) }
+        },
+      }
+
+      schema.properties[settingKeyMaxTokens] = {
+        title: `T_${extensionId}.max-tokens` as any,
+        type: 'number',
+        defaultValue: 256,
+        group: 'openai' as any,
+        required: true,
+        minimum: 4,
+        maximum: 4096,
+        options: {
+          inputAttributes: { placeholder: `T_${extensionId}.max-tokens` }
+        },
+      }
+
+      schema.properties[settingKeyRange] = {
+        title: `T_${extensionId}.range` as any,
+        type: 'number',
+        defaultValue: 256,
+        group: 'openai' as any,
+        required: true,
+        minimum: 10,
+        maximum: 10240,
+        options: {
+          inputAttributes: { placeholder: `T_${extensionId}.range-desc` }
+        },
+      }
+
+      schema.properties[settingKeyArgs] = {
+        title: `T_${extensionId}.args-json` as any,
+        description: `T_${extensionId}.args-json-desc` as any,
+        type: 'string',
+        defaultValue: '{"temperature": 0.3}',
+        group: 'openai' as any,
+        options: {
+          inputAttributes: { placeholder: '{"temperature": 0.3}' }
+        },
+      }
+    })
+
+    ctx.action.registerAction({
+      name: actionName,
+      keys: [ctx.command.CtrlCmd, ctx.command.Alt, 'Period'],
+      handler: () => {
+        ctx.editor.getEditor().getAction('editor.action.inlineSuggest.trigger').run()
+        ctx.editor.getEditor().focus()
+      },
+      when: () => ctx.store.state.showEditor && !ctx.store.state.presentation,
+    })
+
+    ctx.statusBar.tapMenus((menus) => {
+      if (!ctx.store.state.showEditor || ctx.store.state.presentation) {
+        return
+      }
+
+      menus['status-bar-tool']?.list?.push(
+        {
+          id: actionName,
+          type: 'normal',
+          title: ctx.i18n.t(`${extensionId}.openai-complete`),
+          subTitle: ctx.command.getKeysLabel(actionName),
+          onClick: () => ctx.action.getActionHandler(actionName)()
+        },
+      )
+    })
+  }
+})
