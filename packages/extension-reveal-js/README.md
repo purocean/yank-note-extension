20ba8f0d2c079c45b24fdbada3138cd219f6c8f4
purocean <purocean@gmail.com>
2022-07-14 14:43:26 +0800

    feat: add extension-reveal-js extension

---
+++ packages/extension-reveal-js/README.md
@@ -0,0 +1,72 @@
+# Reveal.js
+
+This extension integrates [Reveal.js](https://revealjs.com/) into Yank Note.
+
+## Screenshots
+![image](https://user-images.githubusercontent.com/7115690/178923561-85fa9fb0-d997-4dcd-bc33-48713cecf01f.png)
+
+## Usage
+
+~~~markdown
+---
+revealJsOpts: {
+    theme: 'moon', # theme https://revealjs.com/themes/
+    progress: true, # options https://revealjs.com/config/
+    center: true,
+}
+---
+
+::: section
+## Slide 1 {style="color:red"}
+
+```js
+await new Promise(r => setTimeout(r, 500))
+ctx.ui.useToast().show("info", "HELLOWORLD!")
+console.log("hello world!")
+```
+:::
+
+::: section
+## Horizontal Slide
+
+$$
+\nabla \cdot \vec{\mathbf{B}}  = 0
+$$
+:::
+
+::::: section
+### Vertical Slide
+
+::: section
+### Vertical Slide 1
+
++ Subject{.mindmap}
+    + Topic 1
+    + Topic 2
+    + Topic 3
+    + Topic 4
+
+:::
+::: section
+### Vertical Slide 2
+
+```mermaid
+graph LR
+A[Hard] --> |Text| B(Round)
+```
+
+:::
+::: section
+### Vertical Slide 3
+
+@startuml
+a -> b
+@enduml
+
+:::
+:::::
+~~~
+
+Click "Tool" -> "Present with Reveal.js " in the status bar.
+
+点击状态栏的菜单：“工具” -> “使用 Reveal.js 演示”。
---
+++ packages/extension-reveal-js/src/main.ts
@@ -0,0 +1,107 @@
+import { getExtensionBasePath, registerPlugin } from '@yank-note/runtime-api'
+
+const extensionId = __EXTENSION_ID__
+
+registerPlugin({
+  name: extensionId,
+  register (ctx) {
+    const i18n = ctx.i18n.createI18n({
+      en: {
+        present: 'Present with Reveal.js',
+        print: 'Print with Reveal.js',
+      },
+      'zh-CN': {
+        present: '使用 Reveal.js 演示',
+        print: '使用 Reveal.js 打印',
+      }
+    })
+
+    async function present (print = false) {
+      if (!ctx.getPremium()) {
+        ctx.ui.useToast().show('info', ctx.i18n.t('premium.need-purchase', extensionId))
+        ctx.showPremium()
+        throw new Error('Extension requires premium')
+      }
+
+      const baseUrl = getExtensionBasePath(extensionId)
+
+      const htmlTitle = ctx.store.state.currentFile?.name || 'Reveal.js'
+      const opts = ctx.view.getRenderEnv()?.attributes?.revealJsOpts || {}
+      const theme = opts.theme || 'black'
+
+      const html = `
+        <link rel="stylesheet" href="${baseUrl}/dist/reset.css">
+        <link rel="stylesheet" href="${baseUrl}/dist/reveal.css">
+        <link rel="stylesheet" href="${baseUrl}/dist/theme/${theme}.css">
+        <link rel="stylesheet" href="${baseUrl}/dist/plugin/highlight/monokai.css">
+
+        <div class="reveal">
+          <div id="reveal-slides" class="slides"></div>
+        </div>
+
+        <script src="${baseUrl}/dist/reveal.js"></script>
+        <script src="${baseUrl}/dist/plugin/highlight/highlight.js"></script>
+        <script src="${baseUrl}/dist/plugin/math/math.js"></script>
+      `
+
+      const url = new URL(location.origin + ctx.embed.buildSrc(html, htmlTitle))
+      if (print) {
+        url.searchParams.set('print-pdf', 'true')
+      }
+
+      const win = ctx.env.openWindow(url.toString(), '_blank', { alwaysOnTop: false })
+      if (!win) {
+        throw new Error('Failed to open window')
+      }
+
+      win.window.addEventListener('load', async () => {
+        const content = await ctx.view.getContentHtml({
+          inlineLocalImage: true,
+          includeStyle: true,
+        })
+
+        const tmp = document.createElement('div')
+        tmp.innerHTML = content
+
+        const slides = win.window.document.getElementById('reveal-slides')
+        slides!.innerHTML = tmp.firstElementChild!.innerHTML!
+
+        const Reveal = (win.window as any).Reveal
+        const RevealHighlight = (win.window as any).RevealHighlight
+        const RevealMath = (win.window as any).RevealMath
+        Reveal.initialize({
+          hash: true,
+          controls: true,
+          center: true,
+          ...opts,
+          // Learn about plugins: https://revealjs.com/plugins/
+          plugins: [RevealHighlight, RevealMath.KaTeX]
+        })
+
+        if (print) {
+          setTimeout(() => win.window.print(), 500)
+        }
+      })
+    }
+
+    ctx.statusBar.tapMenus(menus => {
+      menus['status-bar-tool']?.list?.push(
+        { type: 'separator', order: 999 },
+        {
+          id: extensionId + '-present',
+          type: 'normal',
+          title: i18n.t('present'),
+          onClick: () => present(),
+          order: 999,
+        },
+        {
+          id: extensionId + '-print',
+          type: 'normal',
+          title: i18n.t('print'),
+          onClick: () => present(true),
+          order: 999,
+        }
+      )
+    })
+  }
+})
