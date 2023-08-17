ab040faa6a6d99b849e633d335361d9d6926fbcb
purocean <purocean@gmail.com>
2023-08-17 10:06:09 +0800

    feat(extension-git-push): support custom keyboard shortcut

--- packages/extension-git-push/src/main.ts
+++ packages/extension-git-push/src/main.ts
@@ -16,33 +16,44 @@ registerPlugin({
       }
     })
 
+    const actionName = extensionId + '.push'
+
+    async function doAction () {
+      if (ctx.args.FLAG_MAS) {
+        if (await ctx.ui.useModal().confirm({
+          content: i18n.t('not-support'),
+        })) {
+          window.open('https://github.com/purocean/yn/issues/65#issuecomment-1065799677')
+        }
+        return
+      }
+
+      const currentRepo = ctx.store.state.currentRepo
+      const path = currentRepo && currentRepo.path
+      if (path) {
+        let cmd = `pushd "${path}" && git add . && git commit -m update && git push`
+
+        if (!ctx.store.state.showXterm) {
+          cmd += ' && exit'
+        }
+
+        ctx.action.getActionHandler('xterm.run')(cmd)
+      }
+    }
+
+    ctx.action.registerAction({
+      name: actionName,
+      description: i18n.t('git-push'),
+      forUser: true,
+      handler: doAction,
+    })
+
     ctx.statusBar.tapMenus(menus => {
-      menus[extensionId] = {
-        id: extensionId,
+      menus[actionName] = {
+        id: actionName,
         position: 'left',
         title: i18n.t('git-push'),
-        onClick: async () => {
-          if (ctx.args.FLAG_MAS) {
-            if (await ctx.ui.useModal().confirm({
-              content: i18n.t('not-support'),
-            })) {
-              window.open('https://github.com/purocean/yn/issues/65#issuecomment-1065799677')
-            }
-            return
-          }
-
-          const currentRepo = ctx.store.state.currentRepo
-          const path = currentRepo && currentRepo.path
-          if (path) {
-            let cmd = `pushd "${path}" && git add . && git commit -m update && git push`
-
-            if (!ctx.store.state.showXterm) {
-              cmd += ' && exit'
-            }
-
-            ctx.action.getActionHandler('xterm.run')(cmd)
-          }
-        }
+        onClick: doAction,
       }
     })
   }
