42f1725b2cb7eaaa5e3f59d98e06d6e93dc6d054
purocean <purocean@gmail.com>
2024-04-29 21:19:00 +0800

    chore(extension-nes-emulator): update docs & build script

--- packages/extension-nes-emulator/README.md
+++ packages/extension-nes-emulator/README.md
@@ -1,6 +1,6 @@
 # NES Emulator
 
-A NES emulator extension for Yank Note.
+A NES emulator extension for Yank Note. Powered by [jsnes](https://github.com/bfirsh/jsnes)
 
 ## Screenshots
 ![image](https://github.com/purocean/yank-note-extension/assets/7115690/bcc5de92-9343-44e4-ae41-d06737d9baa4)
--- packages/extension-nes-emulator/build-jsnes.sh
+++ packages/extension-nes-emulator/build-jsnes.sh
@@ -6,7 +6,7 @@ sed -i '' 's/git:\/\/github.com\/bfirsh\/jsnes.git/1.2.1/g' package.json;
 sed -i '' 's/BrowserRouter/HashRouter/g' src/App.js;
 sed -i '' 's/config.ROMS\[slug\];/config.ROMS[slug] || {name: "", description: "", url: decodeURIComponent(slug)};/g' src/RunPage.js;
 sed -i '' 's/className="nav-link"/style={{display: "none"}}/g' src/RunPage.js;
-sed -i '' 's/..\/img/.\/img/g' src/ControlsModal.js
+sed -i '' 's/\.\.\/img/.\/img/g' src/ControlsModal.js
 yarn;
 yarn build;
 cp -r build/* ../emulator/;
