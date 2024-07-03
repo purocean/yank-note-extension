d0494e9630eacf093c8daaf91192f093eccc9910
purocean <purocean@gmail.com>
2024-07-03 18:14:38 +0800

    chore(extension-geogebra): update GeoGebra translations

--- packages/extension-geogebra/src/i18n.ts
+++ packages/extension-geogebra/src/i18n.ts
@@ -3,13 +3,13 @@ import { ctx } from '@yank-note/runtime-api'
 
 export default ctx.i18n.createI18n({
   en: {
-    'create-file': 'Create GeoGebra App File',
-    'edit-in-new-window': 'Edit in New Window (GeoGebra App)',
+    'create-file': 'Create GeoGebra File',
+    'edit-in-new-window': 'Edit in New Window (GeoGebra)',
     'file-type': 'File Type: ',
   },
   'zh-CN': {
-    'create-file': '创建 GeoGebra App 文件',
-    'edit-in-new-window': '在新窗口中编辑 (GeoGebra App)',
+    'create-file': '创建 GeoGebra 文件',
+    'edit-in-new-window': '在新窗口中编辑 (GeoGebra)',
     'file-type': '文件类型: ',
   }
 })
